da5f366fe6192eace1840e4029e23f41dcc25f1c
Nguyễn Duy Tân <tannd1904@gmail.com>
2021-06-23 17:24:40 +0700

    Commit 5:24 23/6/2021

---
+++ BackEnd/src/main/java/com/minhtuan/commercemanager/model/DTO/ImageDetailsDTO.java
@@ -0,0 +1,15 @@
+package com.minhtuan.commercemanager.model.DTO;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+@ToString
+public class ImageDTO {
+    private Long id;
+    private Integer imageid;
+    private String image;
+}
